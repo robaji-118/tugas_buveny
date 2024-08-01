3da19dad2ef9c04ccdf4e9fc54b651cf37ac3d89
Rojabi <adhammaoelana@gmail.com>
2024-08-01 11:16:37 +0700

    tugas bu venny

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-// alert(`welcome di pendaftaran sekolah SMKN 1 PURWOSARI`);
-
 function validasi() {
-            var nama = document.forms["formku"]["nama"].value;
-            var nisn = document.forms["formku"]["nisn"].value;
+    var nama = document.forms["formku"]["nama"].value;
+    var nisn = document.forms["formku"]["nisn"].value;
+    var email = document.forms["formku"]["email"].value;
+    var ekspresi = /^([A-Za-z0-9_\-\.])+@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
-            // Validasi nama
-            if (!/^[a-zA-Z]+$/.test(nama)) {
-                alert("Data yang diinputkan untuk nama harus berupa huruf");
-                document.getElementById("nama").focus();
-                document.formku.nama.value = "";
-                return false;
-            }
+    // Validasi nama
+    if (!/^[a-zA-Z]+$/.test(nama)) {
+        alert("Data yang diinputkan untuk nama harus berupa huruf");
+        document.getElementById("nama").focus();
+        document.forms["formku"]["nama"].value = "";
+        return false;
+    }
 
-            // Validasi NISN
-            if (!/^[0-9]+$/.test(nisn)) {
-                alert("Data yang diinputkan untuk NISN harus berupa angka");
-                document.getElementById("nisn").focus();
-                document.formku.nisn.value = "";
-                return false;
-            }
+    // Validasi NISN
+    if (!/^[0-9]+$/.test(nisn)) {
+        alert("Data yang diinputkan untuk NISN harus berupa angka");
+        document.getElementById("nisn").focus();
+        document.forms["formku"]["nisn"].value = "";
+        return false;
+    }
 
-            return true;
-        }
+    // Validasi Email
+    if (email == "" || !ekspresi.test(email)) {
+        alert("Email anda belum benar");
+        document.getElementById("email").focus();
+        return false;
+    } else {
+        alert("Email anda sudah benar");
+    }
+
+    return true;
+}
